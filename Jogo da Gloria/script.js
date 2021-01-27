c55b6a22e15652a504def80408f8c8252f674c6b
diasdiogo461 <diasdiogo461@gmail.com>
2021-01-27 10:30:30 +0000

    yes

--- Jogo da Gloria/script.js
+++ Jogo da Gloria/script.js
@@ -1,23 +1,46 @@
+// ENCONTRAR ID DOS TD'S
+var tdId
+var move = function(td) {
+ 
+  tdId = td.id; 
+  console.log(tdId);
+}
+
 let x= 0;
-let movePlayer;
+//player1
+var img1 = document.createElement("img");
+img1.src="img/1.png";
+img1.style.maxWidth="50px"
+var src = document.getElementById(0);
+src.appendChild(img1);
+/*player2
+var img2 = document.createElement("img");
+img2.src="img/2.png";
+img2.style.maxWidth="50px"
+var src = document.getElementById(0);
+src.appendChild(img2);*/
+
+
 
 function rollDice() {
-    let x = 0;
     x = Math.floor( Math.random()*6 + 1)
     console.log(x);
     document.getElementById("teste").innerHTML = x;
-    return x;
+    
  };
 
+
+
+
+ /*
  var player1;
  var player2;
  var currentPlayer=player1;
 
  if (currentPlayer == player1) {
-    
-  } else {
-    player2Score++;
-  }
-
-  console.log(currentPlayer);
+    player1 = true;
+        
+} else {
+  player2;
+}*/
  
